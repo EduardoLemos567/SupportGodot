d08d4f822660239f99e484ba8b368345ff6879b9
EduardoLemos567 <eduardolemos3@gmail.com>
2025-01-07 18:09:32 -0300

    Updated

--- Resources/Source/Support/MeshBuilder/SurfaceArray.cs
+++ Resources/Source/Support/MeshBuilder/SurfaceArray.cs
@@ -3,63 +3,98 @@ using Godot;
 using GodotArray = Godot.Collections.Array;
 using GodotColor = Godot.Color;
 
-namespace Support.MeshBuilder
+namespace Support.MeshBuilder;
+
+public class SurfaceArray
 {
-    public class SurfaceArray
+    public readonly struct VertexData
     {
-        private readonly List<Vector3> vertices;
-        private readonly List<Vector3> normals;
-        private readonly List<GodotColor> colors;
-        private readonly List<Vector2> uvs;
-        private readonly List<int> indices;
-        public int VertexCount => vertices.Count;
-        public int IndexCount => indices.Count;
-        public SurfaceArray(int expectedVertices, int? expectedIndices = null)
+        public Vector3 Vertex { get; init; }
+        public Vector3 Normal { get; init; }
+        public GodotColor Color { get; init; }
+        public Vector2 Uv { get; init; }
+    }
+    private readonly List<VertexData> data;
+    private readonly List<int> indices;
+    public int VertexCount => data.Count;
+    public int IndexCount => indices.Count;
+    public SurfaceArray(int expectedVertices, int? expectedIndices = null)
+    {
+        expectedIndices ??= expectedVertices;
+        data = new(expectedVertices);
+        indices = new(expectedIndices.Value);
+    }
+    public int AddVertex(in VertexData vertexData)
+    {
+        data.Add(vertexData);
+        return data.Count - 1;
+    }
+    public void SetVertex(int index, in VertexData vertexData)
+    {
+        data[index] = vertexData;
+    }
+    public VertexData GetVertex(int index)
+    {
+        return data[index];
+    }
+    public void AddTriangle(int indexA, int indexB, int indexC)
+    {
+        indices.Add(indexA);
+        indices.Add(indexB);
+        indices.Add(indexC);
+    }
+    private void RecalculateNormalFromTriangle(int firstIndexOfIndex)
+    {
+        int a = indices[firstIndexOfIndex];
+        int b = indices[firstIndexOfIndex + 1];
+        int c = indices[firstIndexOfIndex + 2];
+
+        var da = data[a];
+        var db = data[b];
+        var dc = data[c];
+
+        Vector3 edge1 = db.Vertex - da.Vertex;
+        Vector3 edge2 = dc.Vertex - da.Vertex;
+        Vector3 normal = edge1.Cross(edge2).Normalized();
+
+        data[a] = da with { Normal = (da.Normal + normal).Normalized() };
+        data[b] = db with { Normal = (db.Normal + normal).Normalized() };
+        data[c] = dc with { Normal = (dc.Normal + normal).Normalized() };
+    }
+    public void RecalculateNormals()
+    {
+        for (int i = 0; i < indices.Count; i += 3)
         {
-            expectedIndices ??= expectedVertices;
-            vertices = new(expectedVertices);
-            normals = new(expectedVertices);
-            colors = new(expectedVertices);
-            uvs = new(expectedVertices);
-            indices = new(expectedIndices.Value);
+            RecalculateNormalFromTriangle(i);
         }
-        public int AddVertex(Vector3 vertex, Vector3 normal = default, GodotColor color = default, Vector2 uv = default)
+    }
+    public GodotArray ToArray()
+    {
+        var surfaceArray = new GodotArray();
+        var vertices = new Vector3[data.Count];
+        var normals = new Vector3[data.Count];
+        var colors = new GodotColor[data.Count];
+        var uvs = new Vector2[data.Count];
+        for (var i = 0; i < data.Count; i++)
         {
-            vertices.Add(vertex);
-            normals.Add(normal);
-            colors.Add(color);
-            uvs.Add(uv);
-            return vertices.Count - 1;
+            var d = data[i];
+            vertices[i] = d.Vertex;
+            normals[i] = d.Normal;
+            colors[i] = d.Color;
+            uvs[i] = d.Uv;
         }
-        public void SetVertex(int index, Vector3 vertex, Vector3 normal = default, GodotColor color = default, Vector2 uv = default)
-        {
-            vertices[index] = vertex;
-            normals[index] = normal;
-            colors[index] = color;
-            uvs[index] = uv;
-        }
-        public void AddTriangle(int indexA, int indexB, int indexC)
-        {
-            indices.Add(indexA);
-            indices.Add(indexB);
-            indices.Add(indexC);
-        }
-        private GodotArray ToArray()
-        {
-            var surfaceArray = new GodotArray();
-            surfaceArray.Resize((int)Mesh.ArrayType.Max);
-            surfaceArray[(int)Mesh.ArrayType.Vertex] = vertices.ToArray();
-            surfaceArray[(int)Mesh.ArrayType.Normal] = normals.ToArray();
-            surfaceArray[(int)Mesh.ArrayType.Color] = colors.ToArray();
-            surfaceArray[(int)Mesh.ArrayType.TexUV] = uvs.ToArray();
-            surfaceArray[(int)Mesh.ArrayType.Index] = indices.ToArray();
-            return surfaceArray;
-        }
-        public ArrayMesh AddSurfaceToArrayMesh(ArrayMesh? arrayMesh = null)
-        {
-            arrayMesh ??= new ArrayMesh();
-            arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, ToArray());
-            return arrayMesh;
-        }
+        surfaceArray.Resize((int)Mesh.ArrayType.Max);
+        surfaceArray[(int)Mesh.ArrayType.Vertex] = vertices;
+        surfaceArray[(int)Mesh.ArrayType.Normal] = normals;
+        surfaceArray[(int)Mesh.ArrayType.Color] = colors;
+        surfaceArray[(int)Mesh.ArrayType.TexUV] = uvs;
+        surfaceArray[(int)Mesh.ArrayType.Index] = indices.ToArray();
+        return surfaceArray;
+    }
+    public ArrayMesh AddSurfaceToArrayMesh(ArrayMesh? arrayMesh = null)
+    {
+        arrayMesh ??= new ArrayMesh();
+        arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, ToArray());
+        return arrayMesh;
     }
 }
