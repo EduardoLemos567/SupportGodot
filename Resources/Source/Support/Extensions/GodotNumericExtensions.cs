6aa95a252eab593065366888dac355aca6444fbc
EduardoLemos567 <eduardolemos3@gmail.com>
2025-06-18 12:34:31 -0300

    Updated

--- Resources/Source/Support/Extensions/GodotNumericExtensions.cs
+++ Resources/Source/Support/Extensions/GodotNumericExtensions.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Godot;
 
 namespace Support.Extensions;
 
-public static class Vector2IExtensions
+public static class GodotNumericExtensions
 {
     /// <summary>
     /// Loop through X first than next Y.
@@ -19,5 +20,15 @@ public static class Vector2IExtensions
                 yield return new Vector2I(x, y);
             }
         }
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 ToXZ(this Vector2 vec, float y = 0)
+    {
+        return new Vector3(vec.X, y, vec.Y);
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 TakeXZ(this Vector3 vec)
+    {
+        return new Vector2(vec.X, vec.Z);
     }
 }
